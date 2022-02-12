12c030411d4f0daf22b90046263c63443051e565
VinayMandhaan <vinaymandhaan29@gmail.com>
2022-02-13 04:03:06 +0500

    Form

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { React, useState } from 'react'
 import 'antd/dist/antd.css';
 import UserTable from './components/table/userTable';
 import UserCard from './components/card/userCard';
+import UserForm from './components/form/userForm';
 
 
 
 const App = () => {
   return (
     <div>
-      <UserCard/>
+      <UserForm/>
     </div>
   )
 }
